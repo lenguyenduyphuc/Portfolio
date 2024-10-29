422ccfe86973bc907054cca8c016bcbcb7faf5a7
Phuc <duyphuclenguyen@gmail.com>
2024-10-29 16:34:16 -0400

    Adit hero sections and add about section

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import styles from "./Hero.module.css"
-import {getImageUrl} from "../../utils"
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 children className={styles.title}>
-                    Hello everyone <br/>
-                    I am Nguyen Duy Phuc Le
+                <h1 className={styles.title}>
+                    <span className={styles.line1}>Hello everyone</span>
+                    <br />
+                    <span className={styles.line2}>I am Nguyen Duy Phuc Le</span>
                 </h1>
                 <p className={styles.description}>
-                    I am a growing fullstack developer that loving learning new things and making new connections. Please reach out if you want to know more about me
+                    I am a growing fullstack developer who loves learning new things and making new connections. Please reach out if you want to know more about me.
                 </p>
                 <a href="mailto:duyphuclenguyen@gmail.com" className={styles.contactBtn}>
                     Get in touch with me
@@ -21,10 +22,10 @@ export const Hero = () => {
             <img
                 src={getImageUrl("hero/about.png")}
                 alt="Hero image of me"
-                className={styles.heroIm}
+                className={styles.heroImg}
             />
-            <div className={styles.topBlur}/>
-            <div className={styles.bottomBlur}/>
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
         </section>
-    )
-}
+    );
+};
